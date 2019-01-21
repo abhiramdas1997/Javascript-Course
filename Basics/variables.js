d5868e2aa2264eebd5ed2e2d6bc25fc8164f5e15
Abhiram Das <32413569+abhiramdas1997@users.noreply.github.com>
2019-01-21 14:15:26 +0530

    Add files via upload

---
+++ Basics/variables.js
@@ -0,0 +1,17 @@
+//variable declared used let
+//inside quotes will be taken as strings
+//just directly mention numbers in case of integers
+let name ='john';
+let mark =100;
+let newmark = 50;
+let total=mark+newmark
+console.log(name,total);
+
+
+let firstname='Abhiram';
+let lastname = 'Das';
+console.log(firstname,lastname);
+console.log(firstname+' '+lastname);
+
+let fullname = firstname+lastname;
+console.log(fullname)
