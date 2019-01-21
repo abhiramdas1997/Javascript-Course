e4eb69426dc0fd610875be1f59ec69b7690965e3
Abhiram Das <32413569+abhiramdas1997@users.noreply.github.com>
2019-01-21 22:06:08 +0530

    Add files via upload

---
+++ Basics/nullandbool.js
@@ -0,0 +1,13 @@
+var temp = 50 ;
+console.log('current temperature is '+ temp);//if temperature is not given result will be undefined
+
+//grade system
+//10- great work
+//5-work more
+//2-poor
+//0-failed
+
+//boolena- true/false
+let mark = 10;
+let grade=(mark >= 10);
+console.log(grade);
---
+++ Basics/temperature_conversion_assignment.js
@@ -0,0 +1,3 @@
+var temp_faren = 100;
+var temp_cel = ((temp_faren-32)*(5/9))
+console.log(68+' farenheit in degree celsius is '+ temp_cel);
